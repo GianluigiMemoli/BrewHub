14ad0a36bb24658c8cb8d7878787ca8701fcb24c
GMemoli <gianluigi.memoli@fem.digital>
2025-01-01 17:36:54 +0100

    add: index view

--- brew_hub/BrewHub/urls.py
+++ brew_hub/BrewHub/urls.py
@@ -1,7 +1,16 @@
+from http.client import HTTPResponse
+
+from django.http import HttpResponseRedirect
 from django.urls import path
 from django.urls.conf import include
 
+
+def index(request):
+    return HttpResponseRedirect("/recipe")
+
+
 urlpatterns = [
+    path("", index),
     path("accounts/", include("allauth.urls")),
     path("recipe/", include("recipe.urls")),
 ]
